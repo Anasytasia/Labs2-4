69e04aaf9d4e24c364a63f33a25f5d48f4ecd112
Anasytasia <61075819+Anasytasia@users.noreply.github.com>
2020-05-31 18:40:56 +0300

    Delete test3.js

--- cypress/integration/test3.js
+++
@@ -1,8 +0,0 @@
-describe('The third test',()=>{
-    beforeEach(()=>{
-        cy.visit('/')
-    })
-     it('Click Button', () => {
-     cy.contains('Получить вторую картинку').click()
-     })
-})
