81e5961097874c3dbdbeb5005fec4950925b327b
Anasytasia <61075819+Anasytasia@users.noreply.github.com>
2020-05-31 17:42:19 +0300

    Update photo.js

--- src/components/photo.js
+++ src/components/photo.js
@@ -4,7 +4,7 @@ class Photo extends React.Component {
   render() {
     return(
         <div className="image-container">
-          <img src={this.props.photo_file} width="180" height="180" />
+          <img src={this.props.photo_file} alt='some value' width="180" height="180" />
         </div>
     );
   }
