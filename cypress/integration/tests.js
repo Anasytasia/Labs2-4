f25bd4d7e18f14066a20bb925ce33bc3b46a0a99
Anasytasia <61075819+Anasytasia@users.noreply.github.com>
2020-05-31 18:41:20 +0300

    Add files via upload

---
+++ cypress/integration/tests.js
@@ -0,0 +1,17 @@
+describe('The tests', () => {
+  it('successfully loads', () => {
+    cy.visit('/')
+  })
+
+  it('Api', () => {
+      cy.contains('Получить первую картинку').click()
+      cy.then((response) => {
+          cy.request('http://aws.random.cat/meow')
+          cy.get('img')
+       })
+  })
+
+  it('Click Button', () => {
+  cy.contains('Получить вторую картинку').click()
+  })
+})
